30ce719159b85ab5873a3f617f61c17acc796e40
alatock333-ai <alatock333@gmail.com>
2026-01-28 15:29:40 +0300

    изменены пути файлов
    изменение путей

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-path_progway = r"C:\Users\Asus\Desktop\c++\loGsdk v 2.0\пути для кусков проги.txt"
-path_folderway = r"C:\Users\Asus\Desktop\c++\loGsdk v 2.0\paths.txt"
+path_progway = r"C:\Users\Asus\Desktop\logsdk\loGsdk v 0.4\пути для кусков проги.txt"
+path_folderway = r"C:\Users\Asus\Desktop\logsdk\loGsdk v 0.4\paths.txt"
